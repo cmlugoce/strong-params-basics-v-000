e3565eb0a0933d29ef422e38928940344c9c1e84
Cristina Lugo <cmlugoce@syr.edu>
2018-07-26 14:25:44 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,13 +23,14 @@ def update
   redirect_to post_path(@post)
 end
 
-private
 
-def post_params(*args)
-  params.require(:post).permit(*args)
-end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+	private
+
+def post_params(*args)
+  params.require(:post).permit(*args)
+end
 end
